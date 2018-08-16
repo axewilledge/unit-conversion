28d2138b78742e002a9639b946e00b097f8c08f9
Anjana S Porawagama <axewilledge123@gmail.com>
2018-08-16 09:25:57 +0300

    basic logics for temperature added

--- Unit conversion/Unit conversion/TempViewController.swift
+++ Unit conversion/Unit conversion/TempViewController.swift
@@ -20,6 +20,35 @@ class TempViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var celsiusField: UITextField!
+    @IBOutlet weak var fahrenheitField: UITextField!
+    @IBOutlet weak var kelvinField: UITextField!
+    var celsiusValue = 0.0
+    var fahrenheitValue = 0.0
+    var kelvinValue = 0.0
+    
+    @IBAction func celsius(_ sender: Any) {
+        celsiusValue = Double(celsiusField.text!) ?? 0.0
+        fahrenheitValue = (celsiusValue * (9/5)) + 32
+        kelvinValue = celsiusValue + 273.15
+        fahrenheitField.text = String(fahrenheitValue.rounded(toPlaces: 3))
+        kelvinField.text = String(kelvinValue.rounded(toPlaces: 3))
+        
+    }
+    @IBAction func fahrenheit(_ sender: Any) {
+        fahrenheitValue = Double(fahrenheitField.text!) ?? 0.0
+        celsiusValue = (celsiusValue - 32 ) * (5/9)
+        kelvinValue = celsiusValue + 273.15
+        celsiusField.text = String(celsiusValue.rounded(toPlaces: 3))
+        kelvinField.text = String(kelvinValue.rounded(toPlaces: 3))
+    }
+    @IBAction func kelvin(_ sender: UITextField) {
+        kelvinValue = Double(kelvinField.text!) ?? 0.0
+        celsiusValue = kelvinValue - 273.15
+        fahrenheitValue = (celsiusValue * (9/5)) + 32
+        fahrenheitField.text = String(fahrenheitValue.rounded(toPlaces: 3))
+        celsiusField.text = String(celsiusValue.rounded(toPlaces: 3))
+    }
+    
 }
 
